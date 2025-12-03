23132cb6655e2bc54aff54b6579dda78236ec739
NikollasNC <nikollascardone@gmail.com>
2025-12-03 19:40:13 -0300

    criado tela do usuario

--- front/js/addSorvete.js
+++ front/js/addSorvete.js
@@ -1,8 +1,8 @@
 let botaoCadastro = document.getElementById('botao_cadastro');
 let abaixou = false;
 
-botaoCadastro.addEventListener('click', function() {
-    if(!abaixou) {
+botaoCadastro.addEventListener('click', function () {
+    if (!abaixou) {
         document.getElementById('cadastro').style.height = '450px';
         abaixou = true;
     } else {
@@ -15,6 +15,6 @@ botaoCadastro.addEventListener('click', function() {
 let sorveteImg = document.getElementById('foto_sorvete');
 let inputSorveteImg = document.getElementById('input-file');
 
-inputSorveteImg.onchange = function() {
+inputSorveteImg.onchange = function () {
     sorveteImg.src = URL.createObjectURL(inputSorveteImg.files[0]);
 }
